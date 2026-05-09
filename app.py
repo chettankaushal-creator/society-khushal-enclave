17e9ffdc612c75f0a32837fde28f4239ce6c44a4
chettankaushal-creator <chettankaushal@gmail.com>
2026-05-09 11:13:07 +0530

    Remove debug mode from app.run

--- app.py
+++ app.py
@@ -118,4 +118,4 @@ def init_db():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    app.run(host='0.0.0.0', port=port)
